f0e47d3fe59d885f838e0a8b73a5eab6836c45da
Kelly <kelly.hagan3@gmail.com>
2018-04-02 10:25:38 -0500

    yarn builds

--- client/src/pages/Account/Account.js
+++ client/src/pages/Account/Account.js
@@ -1,4 +1,15 @@
 import React, { Component } from "react";
 import API from "../../utils/API";
-import { Link } from "react-router-dom";
+import { Link, push } from "react-router-dom";
+import { Col, Row, Button, Jumbotron, Grid } from 'react-bootstrap';
+import './Account.css';
 
+
+const Account = props =>
+      <div>
+
+      </div>
+
+
+
+export default Account;
